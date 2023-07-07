c95b907d19aec36297ff9e8cdf0b214c6f833e98
Anjas Dwi <anjasdwi@bareksa.com>
2023-07-08 01:12:52 +0700

    Initial commit

---
+++ Snackbar/Components/SnackbarSimple.swift
@@ -0,0 +1,21 @@
+//
+//  SnackbarComponent.swift
+//  Snackbar
+//
+//  Created by Engineer on 07/07/23.
+//
+
+import Foundation
+import UIKit
+
+public class SnackbarSimple: UIView {
+
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
